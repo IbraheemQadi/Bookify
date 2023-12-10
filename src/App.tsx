22e54dc6d4f26be5b919d3f98d4de6bf8cd57cb1
Ibraheem Qadi <ibra444hem@gmail.com>
2023-12-10 22:38:10 +0200

    Add routes for login, admin, and user pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import PrivateRoutes from "./routes/PrivateRoutes";
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route element={<PrivateRoutes allowedRoles={["admin"]} />}>
+        <Route path="/admin" element={<div>Admin Page</div>} />
+      </Route>
+      <Route element={<PrivateRoutes allowedRoles={["user"]} />}>
+        <Route path="/user" element={<div>User Page</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
