7aeeff4876c32877367ab123c17f2cea7d52bc77
Ibraheem Qadi <ibra444hem@gmail.com>
2023-12-20 20:03:26 +0200

    Some refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { CssBaseline } from "@mui/material";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <CssBaseline />
         <App />
         {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
       </QueryClientProvider>
---
+++ src/pages/Login/Login.stories.ts
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import Login from "./Login";
+
+export default {
+  title: "Pages/Login",
+  component: Login,
+} as Meta;
+
+export const Default = {
+  args: {},
+};
