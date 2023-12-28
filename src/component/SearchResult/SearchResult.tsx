50421e82d24c6f81e996a6ac42d49e444d1fb0b6
Ibraheem Qadi <ibra444hem@gmail.com>
2023-12-28 12:49:43 +0200

    Create SearchResult component with its story

---
+++ src/component/SearchResult/SearchResult.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+import { Hotel as HotelType } from "../../entities/Hotel";
+import Hotel from "../Hotel";
+
+interface Props {
+  hotels: HotelType[];
+}
+
+function SearchResult({ hotels }: Props) {
+  return (
+    <Stack gap={2}>
+      {hotels.map((hotel) => (
+        <Hotel key={hotel.id} hotel={hotel} variant="horizontal" />
+      ))}
+    </Stack>
+  );
+}
+
+export default SearchResult;
