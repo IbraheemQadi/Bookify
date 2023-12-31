0c63387cdac43ddc6e026b49fc1ac1537408124a
Ibraheem Qadi <ibra444hem@gmail.com>
2023-12-31 08:43:35 +0200

    Create HotelGallery component

---
+++ src/data/hotelGallery.ts
@@ -0,0 +1,54 @@
+export const gallery = [
+  {
+    id: 5,
+    url: "https://cdne-hotel-reservation-webapi-dev-001.azureedge.net/web/Plaza%20Hotel.jpg",
+  },
+  {
+    id: 22,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33143786.jpg?k=4d0bca9d9795b80beb2cd9786946e043b23d1372eb633d5855d3aba6343d68d4&o=&hp=1",
+  },
+  {
+    id: 23,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33144464.jpg?k=98997407dcb7957422ca16005b3d19b574ff93b3097e8802fc1f6862eb041e2b&o=&hp=1",
+  },
+  {
+    id: 24,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/31823343.jpg?k=cbd94934282436e4989a72c9bdf725fa45d668902fe7ebc745d8986b73452e18&o=&hp=1",
+  },
+  {
+    id: 25,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33518061.jpg?k=f1817f9efd359ed681c1c5ca21faf9374a124f24788ce8a21c0d3d24a098386f&o=&hp=1",
+  },
+  {
+    id: 26,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33142365.jpg?k=edd59d67083a8d9d3c984a43fb270a7faad8d76ec5bfd82ab64e31293e2aa022&o=&hp=1",
+  },
+  {
+    id: 27,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33142619.jpg?k=0ee749bdc3813831c32c29579017b25697c68efbcf0982d232339361df9bd675&o=&hp=1",
+  },
+  {
+    id: 28,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33142495.jpg?k=9956c92b062724ceb086158d062ca22b4d10a5737fd2fc08879f44d2842d5091&o=&hp=1",
+  },
+  {
+    id: 29,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/33144166.jpg?k=7e773679bcf5b05d10f8eb91d2aeb59db199a354fd31a13dfc07816d93837851&o=&hp=1",
+  },
+  {
+    id: 30,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/31812116.jpg?k=9262e18a69979a8322ffaff1966ac916917d9f112f5502cfd9fed7740e69b850&o=&hp=1",
+  },
+  {
+    id: 31,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/34565601.jpg?k=4e5fec2e716fbf12c63d247f3c06705a8971d3781171036b602a932cf8fdcad5&o=&hp=1",
+  },
+  {
+    id: 32,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/34565621.jpg?k=f1c000203b47441bda0d9cc15d95faaff0c24b07a1a1a87fb5782a615768baa1&o=&hp=1",
+  },
+  {
+    id: 33,
+    url: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/31823255.jpg?k=38a96b3ea99c2f684d425b08d69a256fbc5e57584fb1220feacd836e2a34152c&o=&hp=1",
+  },
+];
