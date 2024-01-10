69aec983d132f5dbf5a4870958be040b18b3fc52
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-10 08:37:55 +0200

    Enhanced Layout component with AppBar, Footer, and conditional Hero rendering

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,12 +1,21 @@
 import { Container } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import AppBar from "../../component/AppBar";
+import Footer from "../../component/Footer";
+import Hero from "../../component/Hero";
 
 function Layout() {
+  const { pathname } = useLocation();
+
   return (
-    <Container>
-      <h1>Layout Page</h1>
-      <Outlet />
-    </Container>
+    <>
+      <AppBar />
+      {pathname === "/" && <Hero />}
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
