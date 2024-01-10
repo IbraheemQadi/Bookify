ba04a66feebb792b6b516fd8d37a88f029127147
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-10 08:24:43 +0200

    Create AppBar component with its story

---
+++ src/component/AppBar/AppBar.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, Story } from "@storybook/react";
+import AppBar from "./AppBar";
+
+export default {
+  title: "Section/AppBar",
+  component: AppBar,
+} as Meta;
+
+const Template: Story = (args) => <AppBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
