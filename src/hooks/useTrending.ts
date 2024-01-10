ab0ead1cee24106607dd0c9ecf632145bae855a2
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-10 08:27:55 +0200

    Create and refactor necessary hooks for the home page

--- src/hooks/useTrending.ts
+++ src/hooks/useTrending.ts
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/apiClient";
 import { Destination } from "../entities/Destination";
 
-const trendingService = new APIClient<Destination>(
+const trendingService = new APIClient<Destination[]>(
   "/home/destinations/trending"
 );
 
 const useTrending = () => {
   return useQuery<Destination[], Error>({
     queryKey: ["trending"],
-    queryFn: trendingService.getAll,
+    queryFn: trendingService.get,
   });
 };
 
