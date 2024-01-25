42c73f376e09923edf7b3129fd98c82a4236fbc1
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-25 14:04:31 +0200

    Story update for components
    - AvailableRoomsGrid
    - HotelGallery

--- src/component/HotelGallery/HotelGallery.stories.tsx
+++ src/component/HotelGallery/HotelGallery.stories.tsx
@@ -1,14 +1,27 @@
-import { Story, Meta } from "@storybook/react";
+import { gallery } from "@/data/hotelGallery";
+import { Meta } from "@storybook/react";
+import { rest } from "msw";
 import HotelGallery from "./HotelGallery";
 
 export default {
   title: "Section/HotelGallery",
   component: HotelGallery,
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          `${import.meta.env.VITE_BASE_URL}/hotels/1/gallery`,
+          (_, res, ctx) => {
+            return res(ctx.json(gallery));
+          }
+        ),
+      ],
+    },
+  },
 } as Meta;
 
-const Template: Story = (args) => <HotelGallery {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  // Pass any necessary props here
+export const Default = {
+  args: {
+    hotelId: 1,
+  },
 };
