c6b5bfcaf5a1631b42c62ff978163296c263c112
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-30 14:19:37 +0200

    Add search and create functionality for CitiesTable component

--- src/component/CitiesTable/CitiesTable.tsx
+++ src/component/CitiesTable/CitiesTable.tsx
@@ -1,7 +1,14 @@
 import { useAdminDrawer } from "@/context/AdminDrawerContext";
 import { City } from "@/entities/City";
 import useCities from "@/hooks/useCities";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Table from "../common/Table";
 
 const sortCities = (cities: City[]) => {
@@ -10,7 +17,11 @@ const sortCities = (cities: City[]) => {
 
 function CitiesTable() {
   const { data, isLoading } = useCities(10, 1);
-  const { openDrawer } = useAdminDrawer();
+  const { openForCreate, openDrawer } = useAdminDrawer();
+
+  const handleCityCreate = () => {
+    openForCreate();
+  };
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -19,6 +30,24 @@ function CitiesTable() {
       <Typography variant="h4" gutterBottom>
         Cities Table
       </Typography>
+      <Paper sx={{ my: 2 }}>
+        <Stack direction="row" gap={1}>
+          <TextField
+            name="search"
+            label="Search"
+            variant="outlined"
+            fullWidth
+            onChange={(e) => console.log(e.target.value)}
+          />
+          <Button
+            onClick={handleCityCreate}
+            variant="contained"
+            color="primary"
+          >
+            Create
+          </Button>
+        </Stack>
+      </Paper>
       <Table
         data={sortCities(data || [])}
         headers={["Id", "Name", "Description"]}
