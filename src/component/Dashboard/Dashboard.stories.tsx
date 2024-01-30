b5eaf6f834e07360a51fa1152414f8222553a623
Ibraheem Qadi <ibra444hem@gmail.com>
2024-01-30 12:02:20 +0200

    Create Dashboard component with navigation

---
+++ src/component/Dashboard/Dashboard.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import Dashboard from "./Dashboard";
+
+export default {
+  title: "Components/Dashboard",
+  component: Dashboard,
+} as Meta;
+
+export const DashboardStory = {
+  args: {},
+};
