6e017024b97440ec47ef1259cd3f020a278b3482
Ibraheem Qadi <ibra444hem@gmail.com>
2024-02-03 10:39:29 +0200

    Added ThemeProvider and theme configuration to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
+import theme from "./theme.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-      {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+        {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
