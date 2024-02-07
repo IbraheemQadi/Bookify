132f7a27d2e7161d104a61d61201cce0d2397a4e
Ibraheem Qadi <ibra444hem@gmail.com>
2024-02-07 20:41:44 +0200

    Introduce CitiesDashboard to contain the cities table and the search bar

---
+++ src/component/AdminSearchBar/AdminSearchBar.tsx
@@ -0,0 +1,47 @@
+import { useAdminDrawer } from "@/context/AdminDrawerContext";
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+interface Props {
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+}
+
+const AdminSearchBar = ({ searchValue, setSearchValue }: Props) => {
+  const { openDrawerForCreate } = useAdminDrawer();
+
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Cities Table
+      </Typography>
+      <Paper sx={{ my: 2 }}>
+        <Stack direction="row" gap={1}>
+          <TextField
+            name="search"
+            label="Search"
+            variant="outlined"
+            value={searchValue}
+            fullWidth
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <Button
+            onClick={() => openDrawerForCreate()}
+            variant="contained"
+            color="primary"
+          >
+            Create
+          </Button>
+        </Stack>
+      </Paper>
+    </Box>
+  );
+};
+
+export default AdminSearchBar;
